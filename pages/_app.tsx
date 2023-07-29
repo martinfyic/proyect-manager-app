baa1e965528403d6f475cc58bfeaa1c221c1723f
Martin Ferreira <martin.f.yic@gmail.com>
2023-07-29 20:55:14 -0300

    refactor: :recycle: theme settings removed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,11 @@
 import type { AppProps } from 'next/app';
-import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import '@/styles/globals.css';
-
-const basicTheme = createTheme({
-	palette: {
-		mode: 'dark',
-	},
-});
+import { darkTheme, ligthTheme } from '@/themes';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<ThemeProvider theme={basicTheme}>
+		<ThemeProvider theme={darkTheme}>
 			<CssBaseline />
 			<Component {...pageProps} />
 		</ThemeProvider>
