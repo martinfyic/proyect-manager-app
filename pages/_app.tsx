8878023bdea8eb479868285e574b450fbbe6781f
Martin Ferreira <martin.f.yic@gmail.com>
2023-07-30 18:10:03 -0300

    perf: :zap: implement UIProvider at pages/_app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import type { AppProps } from 'next/app';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import '@/styles/globals.css';
 import { darkTheme, ligthTheme } from '@/themes';
+import { UIProvider } from '@/context';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<ThemeProvider theme={darkTheme}>
-			<CssBaseline />
-			<Component {...pageProps} />
-		</ThemeProvider>
+		<UIProvider>
+			<ThemeProvider theme={darkTheme}>
+				<CssBaseline />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</UIProvider>
 	);
 }
