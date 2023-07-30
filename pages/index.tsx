522c52ee022e71de7572e61c82b364c4f5e2c623
Martin Ferreira <martin.f.yic@gmail.com>
2023-07-30 19:42:19 -0300

    feat: :lipstick: home page layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,54 @@
+import { NextPage } from 'next';
+import { Card, CardContent, CardHeader, Grid, Typography } from '@mui/material';
 import { Layout } from '@/components/layouts';
-import { Typography } from '@mui/material';
+import { EntryList } from '@/components/ui';
 
-export default function HomePage() {
+const HomePage: NextPage = () => {
 	return (
 		<Layout title='Home Page | Project Manager App'>
-			<Typography
-				variant='h1'
-				color='primary'
+			<Grid
+				container
+				spacing={2}
 			>
-				Proyect Manager APP with Next Js & TypeScript
-			</Typography>
+				<Grid
+					item
+					xs={12}
+					sm={4}
+				>
+					<Card sx={{ height: 'calc( 100vh - 100px )' }}>
+						<CardHeader title='Pending' />
+						<CardContent>
+							<EntryList />
+						</CardContent>
+					</Card>
+				</Grid>
+				<Grid
+					item
+					xs={12}
+					sm={4}
+				>
+					<Card sx={{ height: 'calc( 100vh - 100px )' }}>
+						<CardHeader title='In Progress' />
+						<CardContent>
+							<EntryList />
+						</CardContent>
+					</Card>
+				</Grid>
+				<Grid
+					item
+					xs={12}
+					sm={4}
+				>
+					<Card sx={{ height: 'calc( 100vh - 100px )' }}>
+						<CardHeader title='Completed' />
+						<CardContent>
+							<EntryList />
+						</CardContent>
+					</Card>
+				</Grid>
+			</Grid>
 		</Layout>
 	);
-}
+};
+
+export default HomePage;
