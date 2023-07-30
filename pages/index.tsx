b9d7733174ff09d0756b82c57b70c1ca24b32cac
Martin Ferreira <martin.f.yic@gmail.com>
2023-07-30 18:11:39 -0300

    chore: :art: change Hello Word

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
+import { Layout } from '@/components/layouts';
+import { Typography } from '@mui/material';
+
 export default function HomePage() {
 	return (
-		<>
-			<h1>Hello Word!!!!</h1>
-		</>
+		<Layout title='Home Page | Project Manager App'>
+			<Typography
+				variant='h1'
+				color='primary'
+			>
+				Proyect Manager APP with Next Js & TypeScript
+			</Typography>
+		</Layout>
 	);
 }
