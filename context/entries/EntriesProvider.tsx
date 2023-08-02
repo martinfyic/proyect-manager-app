3a83789fe77a5d82c69bcf0117345b5025b67521
Martin Ferreira <martin.f.yic@gmail.com>
2023-08-02 00:01:28 -0300

    feat: :sparkles: implement addNewEntry at EntriesProvider.tsx

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -12,20 +12,20 @@ const Entries_INITIAL_STATE: EntriesState = {
 		{
 			_id: uuidv4(),
 			description:
-				'Voluptate commodo labore excepteur ut aliqua pariatur minim do in nisi ad velit elit mollit.',
+				'Pending: Voluptate commodo labore excepteur ut aliqua pariatur minim do in nisi ad velit elit mollit.',
 			createdAr: Date.now(),
 			status: 'pending',
 		},
 		{
 			_id: uuidv4(),
 			description:
-				'Sunt aute voluptate pariatur ex in mollit laboris in adipisicing magna dolore excepteur laboris.',
+				'In-Progress: Sunt aute voluptate pariatur ex in mollit laboris in adipisicing magna dolore excepteur laboris.',
 			createdAr: Date.now() - 1000000,
 			status: 'inProgres',
 		},
 		{
 			_id: uuidv4(),
-			description: 'Excepteur nulla et eiusmod ex ipsum eu.',
+			description: 'Finished: Excepteur nulla et eiusmod ex ipsum eu.',
 			createdAr: Date.now() - 1100000,
 			status: 'finished',
 		},
@@ -39,8 +39,19 @@ interface Props {
 export const EntriesProvider: FC<Props> = ({ children }) => {
 	const [state, dispatch] = useReducer(entriesReducer, Entries_INITIAL_STATE);
 
+	const addNewEntry = (description: string) => {
+		const newEntry: Entry = {
+			_id: uuidv4(),
+			description,
+			createdAr: Date.now(),
+			status: 'pending',
+		};
+
+		dispatch({ type: '[Entry] - Add-Entry', payload: newEntry });
+	};
+
 	return (
-		<EntriesContext.Provider value={{ ...state }}>
+		<EntriesContext.Provider value={{ ...state, addNewEntry }}>
 			{children}
 		</EntriesContext.Provider>
 	);
