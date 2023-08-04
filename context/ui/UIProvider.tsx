5d23a43003ed36ad824f25ffbf99e74b8640b281
Martin Ferreira <martin.f.yic@gmail.com>
2023-08-03 21:09:42 -0300

    feat: :sparkles: include isDragging, startDragging, endDragging atc UIProvider.tsx

--- context/ui/UIProvider.tsx
+++ context/ui/UIProvider.tsx
@@ -4,11 +4,13 @@ import { UIContext, uiReducer } from './';
 export interface UIState {
 	sidemenuOpen: boolean;
 	isAddingEntry: boolean;
+	isDragging: boolean;
 }
 
 const UI_INITIAL_STATE: UIState = {
 	sidemenuOpen: false,
 	isAddingEntry: false,
+	isDragging: false,
 };
 
 interface Props {
@@ -30,6 +32,14 @@ export const UIProvider: FC<Props> = ({ children }) => {
 		dispatch({ type: 'UI - Set Add menu', payload: isAdding });
 	};
 
+	const startDragging = () => {
+		dispatch({ type: 'UI - Start Dragging' });
+	};
+
+	const endDragging = () => {
+		dispatch({ type: 'UI - End Dragging' });
+	};
+
 	return (
 		<UIContext.Provider
 			value={{
@@ -37,6 +47,8 @@ export const UIProvider: FC<Props> = ({ children }) => {
 				openSideMenu,
 				closeSideMenu,
 				setAddingMenu,
+				startDragging,
+				endDragging,
 			}}
 		>
 			{children}
