8bec035ce2415c7fa165ed2df2a81072f4c96095
Martin Ferreira <martin.f.yic@gmail.com>
2023-08-15 21:11:39 -0300

    refactor: :recycle: add deletEntry

--- context/entries/EntriesContext.tsx
+++ context/entries/EntriesContext.tsx
@@ -5,6 +5,7 @@ interface ContextProps {
 	entries: Entry[];
 	addNewEntry: (description: string) => void;
 	updateEntry: (entry: Entry, showSnackbar?: boolean) => void;
+	deletEntry: (entry: Entry) => void;
 }
 
 export const EntriesContext: Context<ContextProps> = createContext(
